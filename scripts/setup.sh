c8a04cd3a1537cf213252dd10c7639217b02aba5
Nbw <nbw.adm@gmail.com>
2018-03-14 19:46:01 +0400

    setup.sh shellcheck + "pin" support

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,12 +9,12 @@ set -o nounset
 typeset -a base=( ".gitconfig" ".tmux.conf" ".vim" ".vimrc" ".zlogin" ".zlogout" ".zsh" ".zshrc" )
 typeset mcdir=".config/mc"
 typeset mcini="ini"
-typeset -a dirs=( ".tmp" $mcdir )
+typeset -a dirs=( ".tmp" "$mcdir" )
 typeset -a packages=( "zsh" "vim" "tree" )
 # CONFIGURATION END
 
 main() {
-    local fn=$FUNCNAME
+    local fn=${FUNCNAME[0]}
 
     trap 'except $LINENO' ERR
 
@@ -29,36 +29,38 @@ main() {
 }
 
 dotup() {
-    local fn=$FUNCNAME
+    local fn=${FUNCNAME[0]}
 
-    cd $HOME/.dotfiles
+    cd "$HOME/.dotfiles" || false
 
     echo_info "git pull on .dotfiles"
     git pull
 }
 
 submodules() {
-    local fn=$FUNCNAME
+    local fn=${FUNCNAME[0]}
+    local submodules_list=""
 
-    cd ${HOME}/.dotfiles
+    cd "${HOME}/.dotfiles" || false
 
-    local submodules_list=$(git submodule status)
+    submodules_list=$(git submodule status)
 
     if [[ -z $submodules_list ]]; then
         echo_warn "No submodules found!"
         return 0
     fi
 
-    echo "$submodules_list" | while read line; do
-        typeset -a Sub=($line)
+    echo "$submodules_list" | while read -r line; do
+        typeset -a Sub=()
+        mapfile -t Sub < <(echo "$line")
 
         if [[ ${Sub[0]} =~ -.* ]]; then
             echo_info "git submodule init ${Sub[1]}"
-            git submodule init ${Sub[1]}
+            git submodule init "${Sub[1]}"
         fi
 
         echo_info "git submodule update ${Sub[1]}"
-        git submodule update ${Sub[1]}
+        git submodule update "${Sub[1]}"
 
         unset Sub
     done
@@ -70,7 +72,7 @@ submodules() {
 }
 
 pkginstall() {
-    local fn=$FUNCNAME
+    local fn=${FUNCNAME[0]}
     typeset -i redhat_distribution_major_version=0
 
     if [[ -r /etc/redhat-release ]]; then
@@ -80,53 +82,56 @@ pkginstall() {
     if [[ ! -f /bin/zsh ]]; then
         echo_info "Installing packages..."
         if (( redhat_distribution_major_version >= 21 )); then
-            sudo dnf install ${packages[*]}
+            sudo dnf install "${packages[@]}"
         elif (( redhat_distribution_major_version > 0 )); then
-            sudo yum install ${packages[*]}
+            sudo yum install "${packages[@]}"
         fi
     fi
 }
 
 lnk() {
-    local fn=$FUNCNAME
+    local fn=${FUNCNAME[0]}
 
-    cd $HOME
+    cd "$HOME" || false
 
     touch .viminfo
 
     for f in ${base[*]}; do
-        if [[ ! -h $f ]]; then
-            if [[ -e $f ]]; then
-                mv $f ${f}-$(shuf -i 1000-9999 -n 1).bak
-            fi
+        if ! head "$f" | grep -Fqi 'pinned'; then
+            if [[ ! -h $f ]]; then
+                if [[ -e $f ]]; then
+                    mv "$f" "${f}-$(shuf -i 1000-9999 -n 1).bak"
+                fi
 
-            echo_info_ln "ln -s $f"
-            ln -s .dotfiles/$f $f
+                echo_info_ln "ln -s $f"
+                ln -s ".dotfiles/$f" "$f"
+            fi
         fi
     done
 
     for d in ${dirs[*]}; do
         if [[ ! -d $d ]]; then
-            mkdir -p $d
+            mkdir -p "$d"
         fi
     done
 
-    cd ${HOME}/$mcdir
+    cd "${HOME}/$mcdir" || false
     if [[ ! -h $mcini ]]; then
         echo_info_ln "ln -sf $mcini"
         ln -sf ../../.dotfiles/${mcdir}/$mcini $mcini
     fi
-    cd $HOME
+    cd "$HOME" || false
+
 }
 
 attr() {
-    local fn=$FUNCNAME
+    local fn=${FUNCNAME[0]}
     local attrfile=".prompt.attr"
 
-    cd $HOME
+    cd "$HOME" || false
 
     if [[ ! -f /etc/bash.attr && ! -f $attrfile ]]; then
-        echo > $attrfile << 'EOF'
+        cat > $attrfile << 'EOF'
 # Username color (phy=GREEN, virt=CYAN)
 PR_USER=${PR_BR_BLUE}
 # Root user color (phy=RED, virt=YELLOW)
